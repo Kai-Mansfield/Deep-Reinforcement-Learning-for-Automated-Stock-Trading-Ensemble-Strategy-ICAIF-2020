df2960460900d283a88016e01b090348eabb4036
Kai <50274436+Kai-Mansfield@users.noreply.github.com>
2021-02-06 14:28:03 +0000

    Update preprocessors.py

--- preprocessing/preprocessors.py
+++ preprocessing/preprocessors.py
@@ -2,3 +2,19 @@ import numpy as np
 import pandas as pd
 from stockstats import StockDataFrame as Sdf
 from config import config
+
+def load_dataset(*, file_name: str): -> pd.DataFrame:
+    
+    _data = pd.read_csv(file_name)
+    
+    return data
+
+def data_split(df, start, end):
+  
+    data = df[df.datadate >= start) & (df.datadate < end)]
+    data = data.sort_values(["datadate", "tic"], ignore_index=True)
+    data.index = data.datadate.factorize()[0]
+    
+    return data
+
+  
