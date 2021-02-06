c6a40934d698e3e66839ddd62e66acd4dc4f42f1
Kai <50274436+Kai-Mansfield@users.noreply.github.com>
2021-02-06 13:32:45 +0000

    Update config.py

--- config/config.py
+++ config/config.py
@@ -3,3 +3,9 @@ import pathlib
 import pandas as pd
 import datetime
 import os
+
+training_data = "data/s&p.csv"
+
+now = datetime.datetime.now()
+trained_model_dir = f"trained_models/{now}"
+os.makedirs(trained_model_dir)
